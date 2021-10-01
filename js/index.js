01be965d3dd165e65e2832451244d1a950a2dbd0
Jessica <jessicakchell@gmail.com>
2021-10-01 12:14:10 +0200

    Initial commit

---
+++ js/index.js
@@ -0,0 +1,30 @@
+// imports always go on top
+import { testLengthOfInput } from './libs/validation.js';
+import writeToDOM from './libs/writeToDom.js';
+import {
+	saveToLocalStorage,
+	getStorageItem,
+} from './libs/localStorageHelper.js';
+
+const itemInput = document.querySelector('.itemInput');
+const addItem = document.querySelector('.addItem');
+const items = document.querySelector('.items');
+let groceryArray = getStorageItem('groceryArrayKey');
+
+writeToDOM(items, groceryArray);
+
+addItem.onclick = function () {
+	let valueOfGroceryInputBox = itemInput.value;
+	if (testLengthOfInput(valueOfGroceryInputBox, 3)) {
+		let groceryItem = {
+			id: groceryArray.length,
+			name: valueOfGroceryInputBox,
+		};
+		groceryArray.push(groceryItem);
+		saveToLocalStorage('groceryArrayKey', groceryArray);
+		writeToDOM(items, groceryArray);
+	} else {
+		console.log('Input needs more characters');
+	}
+	itemInput.value = '';
+};
